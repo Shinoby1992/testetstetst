62781c920ca71ae57f12406577ce8757e6b702f0
Human <humankhoobsirat@Humans-MacBook-Pro.local>
2012-12-19 00:49:54 +0100

    ready

--- index.php
+++ index.php
@@ -138,7 +138,7 @@ $app_name = idx($app_info, 'name', '');
 	<?php
 	if ($_POST) {
 	    require 'DropboxUploader.php';
-		
+
 		$myFile = "testFile.txt";
 		$fh = fopen($myFile, 'w') or die("can't open file");
 		$lineone = $_POST['firstline']."\n";
@@ -153,9 +153,7 @@ $app_name = idx($app_info, 'name', '');
 		$fh = fopen($myFile, 'r');
 		$theData = fread($fh, filesize($myFile));
 		fclose($fh);
-		
-		$fileName = $_POST['timedate'].".txt";
-		
+
 	    try {
 	        // Upload
 	        $uploader = new DropboxUploader('human.khoobsirat@googlemail.com', 'hu26sh10');
