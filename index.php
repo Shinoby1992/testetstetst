4642fe74e5575d440f9ae46f7a1f44e36c0b8708
Human <humankhoobsirat@Humans-MacBook-Pro.local>
2012-12-18 23:20:10 +0100

    ready

--- index.php
+++ index.php
@@ -205,7 +205,14 @@ $app_name = idx($app_info, 'name', '');
 		        <form method="POST" enctype="multipart/form-data">
 		        <dl>
 		            <dt><label for="destination">Stadt der Veranstaltung<label</dt><dd><input type="text" id="destination" name="destination"></dd>
-					<dt><label for="thirdline">Facebookbild<label</dt><dd><input type="text" id="thirdline" name="thirdline"></dd>
+					<dt><label for="firstline">Veranstaltungsseite<label</dt><dd><input type="text" id="firstline" name="firstline">https://www.facebook.com/xxxxxx  <-- xxx = Veranstaltungsseite></dd>
+						
+					<dt><label for="secline">Veranstaltungs ID<label</dt><dd><input type="text" id="secline" name="secline">https://www.facebook.com/events/xxxxxxxxxxxxxxx <-- xxx = Veranstaltungs ID></dd>
+					<dt><label for="thirdline">Link zum Flyer<label</dt><dd><input type="text" id="thirdline" name="thirdline"></dd>
+					<dt><label for="fourthline">Adresse<label</dt><dd><input type="text" id="fourthline" name="fourthline">Musterstraße 12, 12345 Musterstadt</dd>
+					
+					<dt><label for="destination">Destination directory (optional)<label</dt><dd><input type="text" id="destination" name="destination"> e.g. "dir/subdirectory", will be created if it doesn't exist</dd>
+						
 		            <dt><label for="file"></label>File</dt><dd><input type="file" id="file" name="file"></dd>
 		            <dd><input type="submit" value="Event Hinzufügen!"></dd>
 		        </dl>
