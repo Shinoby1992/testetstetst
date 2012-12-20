274aa453a6dd6ffc142bee49724ca0bfb1e63e4d
Human <humankhoobsirat@humans-macbook-pro.fritz.box>
2012-12-20 04:41:29 +0100

    nice

--- init.inc.php
+++ init.inc.php
@@ -5,16 +5,14 @@ $exceptions = array('register', 'login');
 $page = substr(end(explode('/', $_SERVER['SCRIPT_NAME'])), 0,-4);
 
 if (in_array($page, $exception) === false){
-	if (isset($_SESSION['username'])) === false){
+	if (isset($_SESSION['username']) === false){
 		header('Location: login.php');
 		die();
 	}
 }
 
-$con = mysql_connect("instance34712.db.xeround.com:3312","app10036823","hu26sh10");
-mysql_select_db("app10036823");
-
-$path = dirname(__FILE__);
+mysql_connect('instance34712.db.xeround.com:3312','app10036823','hu26sh10');
+mysql_select_db('app10036823');
 
 include("user.inc.php");
 ?>
