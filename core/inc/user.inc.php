a7dedb8328927482273ab82de5daf3bbc23ad544
Human <humankhoobsirat@humans-macbook-pro.fritz.box>
2012-12-20 04:18:38 +0100

    nice

--- core/inc/user.inc.php
+++ core/inc/user.inc.php
@@ -9,8 +9,8 @@ function user_exists($user){
 }
 
 //check if the username and password matches
-function valid_credentials ($user, $pass){
-	$$user = mysql_real_escape_string($user);
+function valid_credentials($user, $pass){
+	$user = mysql_real_escape_string($user);
 	
 	$total = mysql_query("SELECT COUNT('user_id') FROM 'users' WHERE 'user_name' = '{$user}' AND 'user_password' = '{$pass}'");
 	
