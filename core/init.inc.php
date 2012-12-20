ef27890a348cb6278da6fc2a5b82f0eacce20578
Human <humankhoobsirat@humans-macbook-pro.fritz.box>
2012-12-20 04:26:53 +0100

    nice

--- core/init.inc.php
+++ core/init.inc.php
@@ -6,14 +6,12 @@ $page = substr(end(explode('/', $_SERVER['SCRIPT_NAME'])), 0,-4);
 
 if (in_array($page, $exception) === false){
 	if (isset($_SESSION['username']) === false){
-		header('Location login.php');
+		header('login.php');
 		die();
 	}
 }
 
 $con = mysql_connect("instance34712.db.xeround.com:3312","app10036823","hu26sh10");
-if (!$con)
-    die('Could not connect: ' . mysql_error());
 mysql_select_db("app10036823");
 
 $path = dirname(__FILE__);
--- logout.php
+++ logout.php
@@ -1,10 +1,8 @@
 <?php
 
 session_start();
-
 $_SESSION = array();
-
 session_destroy();
+header('protected.php');
 
-header('Location: protected.php');
 ?>
