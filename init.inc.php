9a5d624b1a24adcb3af9d75b76f20a2be09d667d
Human <humankhoobsirat@humans-macbook-pro.fritz.box>
2012-12-20 04:42:26 +0100

    nice

--- init.inc.php
+++ init.inc.php
@@ -5,7 +5,7 @@ $exceptions = array('register', 'login');
 $page = substr(end(explode('/', $_SERVER['SCRIPT_NAME'])), 0,-4);
 
 if (in_array($page, $exception) === false){
-	if (isset($_SESSION['username']) === false){
+	if (isset($_SESSION['nameuser']) === false){
 		header('Location: login.php');
 		die();
 	}
