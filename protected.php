0982bbbfdc5ed8418169def3f78c87374e560a54
Human <humankhoobsirat@humans-macbook-pro.fritz.box>
2012-12-20 05:57:39 +0100

    nice

--- protected.php
+++ protected.php
@@ -27,7 +27,7 @@ include('init.inc.php');
 				$fehler[] = 'Veranstaltungsseite darf nicht fehlen.';
 			}
 			if (empty($_POST['secline'])){
-				$fehler[] = 'Veranstaltungs darf nicht fehlen.';
+				$fehler[] = 'Veranstaltungs ID darf nicht fehlen.';
 			}
 			if (empty($_POST['thirdline'])){
 				$fehler[] = 'Link zum Flyer darf nicht fehlen.';
@@ -75,7 +75,7 @@ include('init.inc.php');
 			date_default_timezone_set('CET');
 			$myFile = "log.txt";
 			$fh = fopen($myFile, 'a');
-			$line = "Benutzer: ".$_SESSION['username']." Ordner: ".$_POST['destination']." Dateiname: ".$fileName." Datum: ".date('l jS \of F Y h:i:s A')."\r\n";
+			$line = "Benutzer: ".$_SESSION['username']." Ordner: ".$_POST['destination']." Dateiname: ".$fileName. " Veranstaltungsname: ".$_POST['firstline']." Datum: ".date('l jS \of F Y h:i:s A')."\r\n";
 			fwrite($fh, $line);
 			fclose($fh);
 			
