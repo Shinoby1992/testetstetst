d8acf53ac7198c2d55edfdf07e080111e2329b6c
Human <humankhoobsirat@humans-macbook-pro.fritz.box>
2012-12-20 04:53:01 +0100

    nice

--- init.inc.php
+++ init.inc.php
@@ -5,32 +5,6 @@ $exception = array('register', 'login');
 $page = substr(end(explode('/', $_SERVER['SCRIPT_NAME'])), 0,-4);
 
 if (in_array($page, $exception) === false){
-	echo "ARRAY FALSE";
-	echo "<br>";
-}
-if (in_array($page, $exception) === true){
-	echo "ARRAY TRUE";
-	echo "<br>";
-}
-
-if (isset($_SESSION['username']) === false){
-	echo "USERNAME FALSE";
-	echo "<br>";
-}
-if (isset($_SESSION['username']) === true){
-	echo "USERNAME TRUE";
-	echo "<br>";
-}
-if (isset($_SESSION['blasn']) === true){
-	echo "blasn TRUE";
-	echo "<br>";
-}
-if (isset($_SESSION['blasn']) === false){
-	echo "blasn FALSE";
-	echo "<br>";
-}
-
-if (in_array($page, $exception) === false){
 	if (isset($_SESSION['username']) === false){
 		header('Location: login.php');
 		die();
