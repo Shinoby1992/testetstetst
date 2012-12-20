e23c3dde3f9275e7b9b7ce6e56f5b0515a72ab04
Human <humankhoobsirat@humans-macbook-pro.fritz.box>
2012-12-20 05:03:52 +0100

    nice

--- user.inc.php
+++ user.inc.php
@@ -2,7 +2,7 @@
 //check if the username exists in the database
 function user_exists($user){
 	$user = mysql_real_escape_string($user);
-	$total = mysql_query("SELECT COUNT('user_id') FROM 'users' WHERE 'user_name' = '{$user}'");
+	$total = mysql_query("SELECT COUNT(`user_id`) FROM `users` WHERE `user_name` = '{$user}'");
 	return (mysql_result($total, 0) == '1') ? true : false;
 }
 
@@ -12,7 +12,7 @@ function valid_credentials($user, $pass){
 	$pass = mysql_real_escape_string($pass);
 	echo $user;
 	echo $pass;
-	$total = mysql_query("SELECT COUNT('user_id') FROM 'users' WHERE 'user_name' = '{$user}' AND 'user_password' = '{$pass}'");
+	$total = mysql_query("SELECT COUNT(`user_id`) FROM `users` WHERE `user_name` = '{$user}' AND `user_password` = '{$pass}'");
 	return (mysql_result($total, 0) == '1') ? true : false;
 }
 
