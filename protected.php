334d2dbe9b19ab397cfc0cb75262a43b55db4ff2
Human <humankhoobsirat@humans-macbook-pro.fritz.box>
2013-01-01 22:21:13 +0100

    updated

--- protected.php
+++ protected.php
@@ -89,7 +89,6 @@ include('init.inc.php');
 	        echo '<span style="color: red">Fehler: ' . htmlspecialchars($e->getMessage()) . '</span>';
 	    }
 		unlink('testFile.txt');
-		unlink('log.txt');
 
 	}
 	?>
