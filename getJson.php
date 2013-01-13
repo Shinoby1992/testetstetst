6c97f43540c0be1b70bdb474d2c131164a42ff92
Human <humankhoobsirat@humans-macbook-pro.fritz.box>
2013-01-14 00:49:37 +0100

    nice

--- getJson.php
+++ getJson.php
@@ -3,7 +3,7 @@ session_start();
 $connect = mysql_connect('instance34712.db.xeround.com:3312','app10036823','hu26sh10');
 mysql_select_db('app10036823');
 
-$sth = mysql_query("SELECT * From `users`");
+$sth = mysql_query("SELECT * From `events`");
 $rows = array();
 while($r = mysql_fetch_assoc($sth)) {
     $rows[] = $r;
