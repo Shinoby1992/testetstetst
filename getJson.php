03e77423bbdfcaf34d044bb7c1f6bd79dd34a6aa
Human <humankhoobsirat@Humans-MacBook-Pro.local>
2013-01-14 11:52:39 +0100

    bla

--- getJson.php
+++ getJson.php
@@ -1,4 +1,4 @@
-<?php header('content-type: application/json; charset=utf-8');
+<?php
 session_start();
 $connect = mysql_connect('instance34712.db.xeround.com:3312','app10036823','hu26sh10');
 mysql_select_db('app10036823');
@@ -8,8 +8,7 @@ $rows = array();
 while($r = mysql_fetch_assoc($sth)) {
     $rows[] = $r;
 }
-print $rows;
-
+print json_encode($rows);
 mysql_close($connect);
 
 
