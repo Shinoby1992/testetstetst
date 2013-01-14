bd9835c150a4f5651e94fcb6cf63681db1b2b624
Human <humankhoobsirat@Humans-MacBook-Pro.local>
2013-01-14 12:31:02 +0100

    bla

--- getJson.php
+++ getJson.php
@@ -1,10 +1,14 @@
-<?php header('Content-Type: text/html; charset=utf-8');
+<?php header('Content-Type: application/json; charset=utf-8');
 session_start();
 $connect = mysql_connect('instance34712.db.xeround.com:3312','app10036823','hu26sh10');
 mysql_select_db('app10036823');
 
 mysql_query("SET NAMES 'utf8'"); 
 mysql_query("SET CHARACTER SET 'utf8'");
+
+$cityid = $_GET['stadt_id'];
+
+
 
 $sth = mysql_query("SELECT * From `events`");
 $rows = array();
