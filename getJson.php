919f1729386a8648aaf20f371e1aec670dc714b5
Human <humankhoobsirat@Humans-MacBook-Pro.local>
2013-01-14 12:17:35 +0100

    bla

--- getJson.php
+++ getJson.php
@@ -3,6 +3,8 @@ session_start();
 $connect = mysql_connect('instance34712.db.xeround.com:3312','app10036823','hu26sh10');
 mysql_select_db('app10036823');
 
+mysql_query("SET NAMES 'utf8'"); 
+mysql_query("SET CHARACTER SET 'utf8'"); 
 $sth = mysql_query("SELECT * From `events`");
 $rows = array();
 while($r = mysql_fetch_assoc($sth)) {
