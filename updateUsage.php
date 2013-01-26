d19fa4f129e4956b4fbe55b5d1adda2fffa751ab
Human <humankhoobsirat@Humans-MacBook-Pro.local>
2013-01-26 06:10:42 +0100

    bla

--- updateUsage.php
+++ updateUsage.php
@@ -2,6 +2,9 @@
 $connect = mysql_connect('instance34712.db.xeround.com:3312','app10036823','hu26sh10');
 mysql_select_db('app10036823');
 
+mysql_query("SET NAMES 'utf8'"); 
+mysql_query("SET CHARACTER SET 'utf8'");
+
 $cityid = $_GET['city'];
 $update = sprintf("UPDATE `usage` SET Aufrufe = (Aufrufe + 1) WHERE `Stadt` = '%s'", mysql_real_escape_string($cityid));
 $result = mysql_query($update);
