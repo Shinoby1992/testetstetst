4430460df8b16f570eb768a3b3a278034258ef5f
Human <humankhoobsirat@humans-macbook-pro.fritz.box>
2013-02-06 19:01:30 +0100

    hihi

--- user.inc.php
+++ user.inc.php
@@ -20,9 +20,9 @@ function valid_credentials($user, $pass){
   	  $collection = $db->users;
 	
   	  $qry = array("user_name" => $user,"password" => $pass);
-  	  $result = $collection->findOne($qry);
-  }
-
+  	  $result = $collection->findOne($qry);	
+	
 	return ($result == '1') ? true : false;
+}
 }
 ?>
