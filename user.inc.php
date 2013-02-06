06ba859000ec41600df3ce4430c78206d3b6fd9e
Human <humankhoobsirat@humans-macbook-pro.fritz.box>
2013-02-06 19:08:02 +0100

    hihi

--- user.inc.php
+++ user.inc.php
@@ -22,8 +22,9 @@ function valid_credentials($user, $pass){
   	  $qry = array("user_name" => $user,"password" => $pass);
   	  $result = $collection->findOne($qry);
 
-}	
+
 	
 	return (mysql_result($result, 0) == '1') ? true : false;
+}	
 }
 ?>
