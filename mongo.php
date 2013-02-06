fbb37d8224cdc2de844c5d4c7e6125d41f3b3eb0
Human <humankhoobsirat@humans-macbook-pro.fritz.box>
2013-02-06 19:39:11 +0100

    hihi

--- mongo.php
+++ mongo.php
@@ -26,7 +26,11 @@
 	
 	$qry = array("user_name" => $name,"password" => $password);
 	$result = $collection->findOne($qry);
-	if($result){
+	
+	echo gettype($result);
+	
+	
+	if (gettype($result) === true){
 		$success = "You are successully loggedIn";
 		echo "<li>" .  $success . "</li>";
 	}
