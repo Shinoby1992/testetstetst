7e8cb75e3bdd39bb365adb9921a9d8f08e4372c9
Human <humankhoobsirat@speedport_w723_v_typ_a_1_00_096.(none)>
2013-02-10 23:30:44 +0100

    asd

--- login.php
+++ login.php
@@ -63,12 +63,8 @@ $(document).pngFix( );
 	
 	<!--  start loginbox ................................................................................. -->
 	<div id="loginbox">
-	
-	<!--  start login-inner -->
-	<div id="login-inner">
-		<table border="0" cellpadding="0" cellspacing="0">
 
-		 <?php
+				 <?php
 		if (empty($errors) === false){
     	?>
 			
@@ -85,6 +81,12 @@ $(document).pngFix( );
 		<?php
 		}	
 		?>
+	
+	<!--  start login-inner -->
+	<div id="login-inner">
+		<table border="0" cellpadding="0" cellspacing="0">
+
+
 
 		<form action="" method="post">
 		<tr>
