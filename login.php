fbb30cb200a5ec799b130f9b9f8b328f1fa80909
Human <humankhoobsirat@speedport_w723_v_typ_a_1_00_096.(none)>
2013-02-10 22:40:00 +0100

    b

--- login.php
+++ login.php
@@ -10,10 +10,11 @@ if (isset($_POST['username'], $_POST['password'])){
 	if (empty($_POST['password'])){
 		$errors[] = 'Das Passwort darf nicht leer sein.';
 	}
-	if (!valid_credentials($_POST['username'], $_POST['password'])){
-		$errors[] = 'Benutzername / Passwort falsch.';
-	}
 	
+	//if (!valid_credentials($_POST['username'], $_POST['password'])){
+	//	$errors[] = 'Benutzername / Passwort falsch.';
+	//}
+
 	if (empty($errors)){
 		$_SESSION['username'] = htmlentities($_POST['username']);
 		
@@ -24,18 +25,50 @@ if (isset($_POST['username'], $_POST['password'])){
 }	
 ?>
 
-<!DOCTYPE html>
-<html xmlns:fb="http://ogp.me/ns/fb#" lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes" />
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>DiscGo Official Manager</title>
+<link rel="stylesheet" href="css/screen.css" type="text/css" media="screen" title="default" />
+<!--  jquery core -->
+<script src="js/jquery/jquery-1.4.1.min.js" type="text/javascript"></script>
 
-    <title>DiscGo - Manager</title>
-    <link rel="stylesheet" href="stylesheets/screen.css" media="Screen" type="text/css" />
-    <link rel="stylesheet" href="stylesheets/mobile.css" media="handheld, only screen and (max-width: 480px), only screen and (max-device-width: 480px)" type="text/css" />
-  </head>
-  <body>
-	  <?php
+<!-- Custom jquery scripts -->
+<script src="js/jquery/custom_jquery.js" type="text/javascript"></script>
+
+<!-- MUST BE THE LAST SCRIPT IN <HEAD></HEAD></HEAD> png fix -->
+<script src="js/jquery/jquery.pngFix.pack.js" type="text/javascript"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+$(document).pngFix( );
+});
+</script>
+</head>
+<body id="login-bg"> 
+ 
+<!-- Start: login-holder -->
+<div id="login-holder">
+
+<div id="logo-changing">
+	<a href="index.html"><img src="images/shared/logo2.png" width="120" height="120" alt="" /></a>
+</div>
+	
+	<!-- start logo -->
+	<div id="logo-login">
+	</div>
+	<!-- end logo -->
+	
+	<div class="clear"></div>
+	
+	<!--  start loginbox ................................................................................. -->
+	<div id="loginbox">
+	
+	<!--  start login-inner -->
+	<div id="login-inner">
+		<table border="0" cellpadding="0" cellspacing="0">
+
+		 <?php
 		if (empty($errors) === false){
     	?>
 			
@@ -52,19 +85,28 @@ if (isset($_POST['username'], $_POST['password'])){
 		<?php
 		}	
 		?>
-	  <section id="samples" class="clearfix">	  
-		  <form action="" method="post">
-		<p>
-			<label for="username">Username:</label>
-			<input type="text" name="username" id="username" />
-		</p>
-		<p>
-			<label for="password">Passwort:</label>
-			<input type="password" name="password" id="password" />
-		</p>
-		<p>
-			<input type="submit" value="Einloggen" />
-		</p>
-	</form>
+
+		<form action="" method="post">
+		<tr>
+			<th>Benutzer</th>
+			<td><input type="text"  name="username" id="username" class="login-inp" /></td>
+		</tr>
+		<tr>
+			<th>Passwort</th>
+			<td><input type="password" name="password" id="password" value="************"  onfocus="this.value=''" class="login-inp" /></td>
+		</tr>
+		<tr>
+			<th></th>
+			<td><input type="submit" name="submitbu" class="submit-login"  /></td>
+		</tr>
+		</form>
+		</table>
+	</div>
+ 	<!--  end login-inner -->
+	<div class="clear"></div>
+ </div>
+
+</div>
+<!-- End: login-holder -->
 </body>
-	</section>
+</html>
--- protected.php
+++ protected.php
@@ -1,20 +1,182 @@
 <?php 
 include('init.inc.php');
 ?>
-<!DOCTYPE html>
-<html xmlns:fb="http://ogp.me/ns/fb#" lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes" />
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>DiscGo Official Manager</title>
 
-    <title>DiscGo - Manager</title>
-    <link rel="stylesheet" href="stylesheets/screen.css" media="Screen" type="text/css" />
-    <link rel="stylesheet" href="stylesheets/mobile.css" media="handheld, only screen and (max-width: 480px), only screen and (max-device-width: 480px)" type="text/css" />
-  </head>
-  <body>
-	  
-	  
-	  
+<link rel="stylesheet" href="css/screen.css" type="text/css" media="screen" title="default" />
+<!--[if IE]>
+<link rel="stylesheet" media="all" type="text/css" href="css/pro_dropline_ie.css" />
+<![endif]-->
+
+<!--  jquery core -->
+<script src="js/jquery/jquery-1.4.1.min.js" type="text/javascript"></script>
+
+<!--  checkbox styling script -->
+<script src="js/jquery/ui.core.js" type="text/javascript"></script>
+<script src="js/jquery/ui.checkbox.js" type="text/javascript"></script>
+<script src="js/jquery/jquery.bind.js" type="text/javascript"></script>
+<script type="text/javascript">
+$(function(){
+	$('input').checkBox();
+	$('#toggle-all').click(function(){
+ 	$('#toggle-all').toggleClass('toggle-checked');
+	$('#mainform input[type=checkbox]').checkBox('toggle');
+	return false;
+	});
+});
+</script>  
+
+<![if !IE 7]>
+
+<!--  styled select box script version 1 -->
+<script src="js/jquery/jquery.selectbox-0.5.js" type="text/javascript"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+	$('.styledselect').selectbox({ inputClass: "selectbox_styled" });
+});
+</script>
+ 
+
+<![endif]>
+
+<!--  styled select box script version 2 --> 
+<script src="js/jquery/jquery.selectbox-0.5_style_2.js" type="text/javascript"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+	$('.styledselect_form_1').selectbox({ inputClass: "styledselect_form_1" });
+	$('.styledselect_form_2').selectbox({ inputClass: "styledselect_form_2" });
+});
+</script>
+
+<!--  styled select box script version 3 --> 
+<script src="js/jquery/jquery.selectbox-0.5_style_2.js" type="text/javascript"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+	$('.styledselect_pages').selectbox({ inputClass: "styledselect_pages" });
+});
+</script>
+
+<!--  styled file upload script --> 
+<script src="js/jquery/jquery.filestyle.js" type="text/javascript"></script>
+<script type="text/javascript" charset="utf-8">
+  $(function() {
+      $("input.file_1").filestyle({ 
+          image: "images/forms/choose-file.gif",
+          imageheight : 21,
+          imagewidth : 78,
+          width : 310
+      });
+  });
+</script>
+
+<!-- Custom jquery scripts -->
+<script src="js/jquery/custom_jquery.js" type="text/javascript"></script>
+ 
+<!-- Tooltips -->
+<script src="js/jquery/jquery.tooltip.js" type="text/javascript"></script>
+<script src="js/jquery/jquery.dimensions.js" type="text/javascript"></script>
+<script type="text/javascript">
+$(function() {
+	$('a.info-tooltip ').tooltip({
+		track: true,
+		delay: 0,
+		fixPNG: true, 
+		showURL: false,
+		showBody: " - ",
+		top: -35,
+		left: 5
+	});
+});
+</script> 
+
+
+<!--  date picker script -->
+<link rel="stylesheet" href="css/datePicker.css" type="text/css" />
+<script src="js/jquery/date.js" type="text/javascript"></script>
+<script src="js/jquery/jquery.datePicker.js" type="text/javascript"></script>
+<script type="text/javascript" charset="utf-8">
+        $(function()
+{
+
+// initialise the "Select date" link
+$('#date-pick')
+	.datePicker(
+		// associate the link with a date picker
+		{
+			createButton:false,
+			startDate:'01/01/2005',
+			endDate:'31/12/2020'
+		}
+	).bind(
+		// when the link is clicked display the date picker
+		'click',
+		function()
+		{
+			updateSelects($(this).dpGetSelected()[0]);
+			$(this).dpDisplay();
+			return false;
+		}
+	).bind(
+		// when a date is selected update the SELECTs
+		'dateSelected',
+		function(e, selectedDate, $td, state)
+		{
+			updateSelects(selectedDate);
+		}
+	).bind(
+		'dpClosed',
+		function(e, selected)
+		{
+			updateSelects(selected[0]);
+		}
+	);
+	
+var updateSelects = function (selectedDate)
+{
+	var selectedDate = new Date(selectedDate);
+	$('#d option[value=' + selectedDate.getDate() + ']').attr('selected', 'selected');
+	$('#m option[value=' + (selectedDate.getMonth()+1) + ']').attr('selected', 'selected');
+	$('#y option[value=' + (selectedDate.getFullYear()) + ']').attr('selected', 'selected');
+
+	
+}
+// listen for when the selects are changed and update the picker
+$('#d, #m, #y')
+	.bind(
+		'change',
+		function()
+		{
+			var d = new Date(
+						$('#y').val(),
+						$('#m').val()-1,
+						$('#d').val()
+					);
+			$('#date-pick').dpSetSelected(d.asString());
+		}
+	);
+
+// default the position of the selects to today
+var today = new Date();
+updateSelects(today.getTime());
+
+// and update the datePicker to reflect it...
+$('#d').trigger('change');
+});
+</script>
+
+<!-- MUST BE THE LAST SCRIPT IN <HEAD></HEAD></HEAD> png fix -->
+<script src="js/jquery/jquery.pngFix.pack.js" type="text/javascript"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+$(document).pngFix( );
+});
+</script>
+</head>
+<body> 
 	<?php
 	if ($_POST) {
 	    require 'DropboxUploader.php';
@@ -137,24 +299,320 @@ include('init.inc.php');
 	?>
 
 	
-	<section id="samples" class="clearfix">
-		<p>
-			Du bist eingeloggt als <?php echo $_SESSION['username'];?>
-		</p>
-		<p>
-			<a href="logout.php">Logout?</a>
-		</p>
+<!-- Start: page-top-outer -->
+<div id="page-top-outer">
+	<!-- Start: page-top -->
+	<div id="page-top">
+		<!-- start logo -->
+		<div id="logo">
+			<a href=""><img src="images/shared/logo2.png" width="80" height="80" alt="" /></a>
+			<p style="font-weight:bold;color:#FFFFFF;letter-spacing:2pt;word-spacing:3pt;font-size:32px;text-align:center;font-family:arial, helvetica, sans-serif;line-height:1;">DiscGo</p>
+		</div>
+		<!-- end logo -->
+	</div>
+</div>
+
+<div class="clear">&nbsp;</div>
+
+<!--  start nav-outer-repeat................................................................................................. START -->
+<div class="nav-outer-repeat"> 
+<!--  start nav-outer -->
+<div class="nav-outer"> 
+		<!-- start nav-right -->
+		<div id="nav-right">
+			<div class="nav-divider">&nbsp;</div>
+			<div class="showhide-account">
+				<!-- <img src="images/shared/nav/nav_myaccount.gif" width="93" height="14" alt="" />-->
+				<h2 style="color:white"><?php echo $_SESSION['username'];?></h2>
+			</div>
+			<div class="nav-divider">&nbsp;</div>
+			<a href="logout.php" id="logout"><img src="images/shared/nav/nav_logout.gif" width="64" height="14" alt="" /></a>
+			<div class="clear">&nbsp;</div>
+		</div>
+		<!-- end nav-right -->
+
+		<!--  start nav -->
+		<div class="nav">
+		<div class="table">
+		                    
+		<ul class="current"><li><a href="#nogo"><b>Veranstaltung</b><!--[if IE 7]><!--></a><!--<![endif]-->
+		<!--[if lte IE 6]><table><tr><td><![endif]-->
+		<div class="select_sub show">
+			<ul class="sub">
+				<li class="sub_show"><a href="#nogo">Hinzufügen</a></li>
+			</ul>
+		</div>
+		<!--[if lte IE 6]></td></tr></table></a><![endif]-->
+		</li>
+		</ul>
+		<div class="clear"></div>
+		</div>
+		<div class="clear"></div>
+		</div>
+
+	<!--  start nav -->
+	</div>
+	<div class="clear"></div>
+	<!--  start nav-outer -->
+	</div>
+	<!--  start nav-outer-repeat................................................... END -->
+
+	<div class="clear"></div>
+	 
+	<!-- start content-outer -->
+	<div id="content-outer">
+	<!-- start content -->
+	<div id="content">
+
+		<!--  start page-heading -->
+		<div id="page-heading"><h1>Veranstaltung Hinzufügen</h1></div>
+		<!-- end page-heading -->
+
+		<table border="0" width="100%" cellpadding="0" cellspacing="0" id="content-table">
+		<tr>
+			<th rowspan="3" class="sized"><img src="images/shared/side_shadowleft.jpg" width="20" height="300" alt="" /></th>
+			<th class="topleft"></th>
+			<td id="tbl-border-top">&nbsp;</td>
+			<th class="topright"></th>
+			<th rowspan="3" class="sized"><img src="images/shared/side_shadowright.jpg" width="20" height="300" alt="" /></th>
+		</tr>
+<tr>
+	<td id="tbl-border-left"></td>
+	<td>
+	<!--  start content-table-inner -->
+	<div id="content-table-inner">
+
+	<table border="0" width="100%" cellpadding="0" cellspacing="0">
+	<tr valign="top">
+	<td>
+
+		<!--  start step-holder -->
+		<div id="step-holder">
+			<div class="step-no">1</div>
+			<div class="step-dark-left"><a href="">Veranstaltung</a></div>
+			<div class="step-dark-right">&nbsp;</div>
+			<div class="step-no-off">2</div>
+			<div class="step-light-left">Vorschau</div>
+			<div class="step-light-right">&nbsp;</div>
+			<div class="step-no-off">3</div>
+			<div class="step-light-left">Hinzufügen</div>
+			<div class="step-light-round">&nbsp;</div>
+			<div class="clear"></div>
+		</div>
+		<!--  end step-holder -->
+		<!-- start id-form -->
+		<table border="0" cellpadding="0" cellspacing="0"  id="id-form">
+
+		<form method="POST" enctype="multipart/form-data">
+		<tr>
+			<th valign="top">Stadt:</th>
+			<td><input type="text" id="destination" name="destination" class="inp-form-error" /></td>
+			<td>
+			<div class="error-left"></div>
+			<div class="error-inner">Muss angegeben werden</div>
+			</td>
+		</tr>
+		<tr>
+			<th valign="top">Datum:</th>
+			<td><input type="text" id="timedate" name="timedate" class="inp-form-error" /></td>
+			<td>
+			<div class="error-left"></div>
+			<div class="error-inner">Muss angegeben werden</div>
+			</td>
+		</tr>
+		<tr>
+			<th valign="top">Facebook Seite:</th>
+			<td><input type="text" id="firstline" name="firstline" class="inp-form-error" /></td>
+			<td>
+			<div class="error-left"></div>
+			<div class="error-inner">Muss angegeben werden</div>
+			</td>
+		</tr>
+		<tr>
+			<th valign="top">Facebook EventID:</th>
+			<td><input type="text" id="secline" name="secline" class="inp-form-error" /></td>
+			<td>
+			<div class="error-left"></div>
+			<div class="error-inner">Muss angegeben werden</div>
+			</td>
+		</tr>
+		<tr>
+			<th valign="top">Adresse:</th>
+			<td><input type="text" id="fourthline" name="fourthline" class="inp-form-error" /></td>
+			<td>
+			<div class="error-left"></div>
+			<div class="error-inner">Muss angegeben werden</div>
+			</td>
+		</tr>
+		<tr>
+			<th valign="top">Informationen:</th>
+			<td><textarea rows="" cols="" id="fifthline" name="fifthline" class="form-textarea"></textarea></td>
+			<td>
+			<div class="error-left"></div>
+			<div class="error-inner">Muss angegeben werden</div>
+			</td>
+			<td></td>
+		</tr>
+		<tr>
+			<th>Link zum Flyer</th>
+			<td><input type="text" id="thirdline" name="thirdline" class="inp-form-error" /></td>
+			<td>
+			<div class="error-left"></div>
+			<div class="error-inner">Muss angegeben werden</div>
+			<div class="bubble-left"></div>
+			<div class="bubble-inner">Optimale Größe 680x960</div>
+			<div class="bubble-right"></div>
+			</td>
+		</td>
+		</tr>
+		<tr>
+		<th>&nbsp;</th>
+		<td valign="top">
+			<input type="submit" value="" class="form-submit" />
+		</td>
+		<td></td>
+		</tr>
+
+		</form>
+	</table>
+	<!-- end id-form  -->
+
+	</td>
+	<td>
+
+	<!--  start related-activities -->
+	<div id="related-activities">
 		
-		        <form method="POST" enctype="multipart/form-data">
-		        <dl>
-					<dt><label for="timedate">Datum der Veranstaltung<label</dt><dd><input type="text" id="timedate" name="timedate">year-mm-dd (2013-01-31)</dd>
-		            <dt><label for="destination">Stadt der Veranstaltung<label</dt><dd><input type="text" id="destination" name="destination"></dd>
-					<dt><label for="firstline">Veranstaltungsseite<label</dt><dd><input type="text" id="firstline" name="firstline">https://www.facebook.com/xxxxxx  <-- xxx = Veranstaltungsseite</dd>
-						
-					<dt><label for="secline">Veranstaltungs ID<label</dt><dd><input type="text" id="secline" name="secline">https://www.facebook.com/events/xxxxx <-- xxx = Veranstaltungs ID</dd>
-					<dt><label for="thirdline">Link zum Flyer<label</dt><dd><input type="text" id="thirdline" name="thirdline">Größe 680x960</dd>
-					<dt><label for="fourthline">Adresse<label</dt><dd><input type="text" id="fourthline" name="fourthline">Musterstraße 12, 12345 Musterstadt</dd>
-		            <dt><label for="fifthline">Informationen<label</dt><dd><textarea id="fifthline" name="fifthline" cols="25" rows="5"></textarea>Einlass Ab 22Uhr;Eintritt 8€, MdvZ 3€;usw (Zeilen mit ; trennen. Nicht neue Zeilen erstellen(Entertaste)!!!!)</dd>
-					<dd><input type="submit" value="Event Hinzufügen!"></dd>
-		        </dl>
-	</section>
+		<!--  start related-act-top -->
+		<div id="related-act-top">
+		<img src="images/forms/header_related_act.gif" width="271" height="43" alt="" />
+		</div>
+		<!-- end related-act-top -->
+		
+		<!--  start related-act-bottom -->
+		<div id="related-act-bottom">
+		
+			<!--  start related-act-inner -->
+			<div id="related-act-inner">
+				<div class="left"><a href=""><img src="images/forms/icon_plus.gif" width="21" height="21" alt="" /></a></div>
+				<div class="right">
+					<h5>Stadt</h5>
+					<ul class="greyarrow">
+					<li>Musterstadt</li> 
+					</ul>
+				</div>
+
+				<div class="clear"></div>
+				<div class="lines-dotted-short"></div>
+
+				<div class="left"><a href=""><img src="images/forms/icon_plus.gif" width="21" height="21" alt="" /></a></div>
+				<div class="right">
+					<h5>Datum</h5>
+					<ul class="greyarrow">
+					Veranstaltungs Datum
+					<li>year-mm-dd (2013-01-31)</li>
+					</ul>
+				</div>
+
+				<div class="clear"></div>
+				<div class="lines-dotted-short"></div>
+
+				<div class="left"><a href=""><img src="images/forms/icon_plus.gif" width="21" height="21" alt="" /></a></div>
+				<div class="right">
+					<h5>Facebook Seite</h5>
+					<ul class="greyarrow">
+					https://www.facebook.com/xxxxxx
+					<li>xxxxxx = Facebook Seite</li> 
+					</ul>
+				</div>
+				
+				<div class="clear"></div>
+				<div class="lines-dotted-short"></div>
+				
+				<div class="left"><a href=""><img src="images/forms/icon_plus.gif" width="21" height="21" alt="" /></a></div>
+				<div class="right">
+					<h5>Facebook EventID</h5>
+					<ul class="greyarrow">
+					https://www.facebook.com/events/xxxxxx
+					<li>xxxxxx = Veranstaltungs ID</li> 
+					</ul>
+				</div>
+				
+				<div class="clear"></div>
+				<div class="lines-dotted-short"></div>
+				
+				<div class="left"><a href=""><img src="images/forms/icon_plus.gif" width="21" height="21" alt="" /></a></div>
+				<div class="right">
+					<h5>Adresse</h5>
+					<ul class="greyarrow">
+					<li>Musterstraße 15, 12345 Musterstadt</li> 
+					</ul>
+				</div>
+
+				<div class="clear"></div>
+				<div class="lines-dotted-short"></div>
+
+				<div class="left"><a href=""><img src="images/forms/icon_plus.gif" width="21" height="21" alt="" /></a></div>
+				<div class="right">
+					<h5>Informationen</h5>
+					<ul class="greyarrow">
+					Bsp: Einlass ab 22Uhr;Eintritt 8€; MdvZ 3€
+					<li>Zum Trennen ; benutzen keine neuen Zeilen(Entertaste)!!!</li> 
+					</ul>
+				</div>
+
+				<div class="clear"></div>
+				<div class="lines-dotted-short"></div>
+
+				<div class="left"><a href=""><img src="images/forms/icon_plus.gif" width="21" height="21" alt="" /></a></div>
+				<div class="right">
+					<h5>Link zum Flyer</h5>
+					<ul class="greyarrow">
+					<li>Kompletten Link zum Bild</li> 
+					</ul>
+				</div>
+
+				<div class="clear"></div>
+				
+			</div>
+			<!-- end related-act-inner -->
+			<div class="clear"></div>
+		
+		</div>
+		<!-- end related-act-bottom -->
+	
+	</div>
+	<!-- end related-activities -->
+
+</td>
+</tr>
+
+<tr>
+<td><img src="images/shared/blank.gif" width="695" height="1" alt="blank" /></td>
+<td></td>
+</tr>
+</table>
+ 
+<div class="clear"></div>
+</div>
+<!--  end content-table-inner  -->
+</td>
+<td id="tbl-border-right"></td>
+</tr>
+<tr>
+	<th class="sized bottomleft"></th>
+	<td id="tbl-border-bottom">&nbsp;</td>
+	<th class="sized bottomright"></th>
+</tr>
+</table>
+<!-- start footer -->         
+<div id="footer">
+	<!--  start footer-left -->
+	<div id="footer-left">
+	DiscGo &copy; Copyright Human Khoobsirat 2013. All rights reserved.</div>
+	<!--  end footer-left -->
+	<div class="clear">&nbsp;</div>
+</div>
+<!-- end footer -->
+</body>
+</html>
