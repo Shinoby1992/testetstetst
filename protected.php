82843cde4842ef75b6f5eef34fbcc6422ece0713
Human <humankhoobsirat@speedport_w723_v_typ_a_1_00_096.(none)>
2013-02-11 01:25:53 +0100

    asd

--- protected.php
+++ protected.php
@@ -386,18 +386,22 @@ $(document).pngFix( );
 
 		<?php 
 		if (empty($fehler) === false){
+			foreach ($fehler as $fehlers){			
 		?>
 		<!--  start message-red -->
 		<div id="message-red">
 		<table border="0" width="100%" cellpadding="0" cellspacing="0">
 		<tr>
-			<td class="red-left">Fehler. <a href="">Please try again.</a></td>
+			<?php
+				echo "<td class="red-left">Fehler. <a href="">{$fehlers}</a></td>"
+			?>
 			<td class="red-right"><a class="close-red"><img src="images/table/icon_close_red.gif"   alt="" /></a></td>
 		</tr>
 		</table>
 		</div>
 		<!--  end message-red -->
 		<?php
+			}
 		}
 		?>
 
