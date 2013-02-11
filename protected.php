40b071e062d59c0ce8042d249c35f2b1c8537798
Human <humankhoobsirat@Humans-MacBook-Pro.local>
2013-02-11 11:18:34 +0100

    s

--- protected.php
+++ protected.php
@@ -366,16 +366,16 @@ $(document).pngFix( );
 		<div id="page-heading"><h1>Veranstaltung Hinzufügen</h1></div>
 		<!-- end page-heading -->
 
-		<!--Steps
+
 		<table border="0" width="100%" cellpadding="0" cellspacing="0" id="content-table">
-		//<tr>
+		<tr>
 			<th rowspan="3" class="sized"><img src="images/shared/side_shadowleft.jpg" width="20" height="300" alt="" /></th>
 			<th class="topleft"></th>
 			<td id="tbl-border-top">&nbsp;</td>
 			<th class="topright"></th>
 			<th rowspan="3" class="sized"><img src="images/shared/side_shadowright.jpg" width="20" height="300" alt="" /></th>
 		</tr>
-		-->
+		
 <tr>
 	<td id="tbl-border-left"></td>
 	<td>
@@ -425,7 +425,7 @@ $(document).pngFix( );
 		<?php
 			}
 		?>
-		<!--  start step-holder -->
+		<!--  start step-holder 
 		<div id="step-holder">
 			<div class="step-no">1</div>
 			<div class="step-dark-left"><a href="">Veranstaltung</a></div>
@@ -438,7 +438,7 @@ $(document).pngFix( );
 			<div class="step-light-round">&nbsp;</div>
 			<div class="clear"></div>
 		</div>
-		<!--  end step-holder -->
+		end step-holder -->
 		<!-- start id-form -->
 		<table border="0" cellpadding="0" cellspacing="0"  id="id-form">
 
