270ea874704b17fb64d3f9fdcf426e5690a01e08
Human <humankhoobsirat@speedport_w723_v_typ_a_1_00_096.(none)>
2013-02-11 01:22:56 +0100

    asd

--- protected.php
+++ protected.php
@@ -384,16 +384,26 @@ $(document).pngFix( );
 	<tr valign="top">
 	<td>
 
-							<!--  start message-red -->
-				<div id="message-red">
-				<table border="0" width="100%" cellpadding="0" cellspacing="0">
-				<tr>
-					<td class="red-left">Error. <a href="">Please try again.</a></td>
-					<td class="red-right"><a class="close-red"><img src="images/table/icon_close_red.gif"   alt="" /></a></td>
-				</tr>
-				</table>
-				</div>
-				<!--  end message-red -->
+		<?php 
+		if (empty($fehler) === false){
+		?>
+		<!--  start message-red -->
+		<div id="message-red">
+		<table border="0" width="100%" cellpadding="0" cellspacing="0">
+		<tr>
+			<td class="red-left">Fehler. <a href="">Please try again.</a></td>
+			<td class="red-right"><a class="close-red"><img src="images/table/icon_close_red.gif"   alt="" /></a></td>
+		</tr>
+		</table>
+		</div>
+		<!--  end message-red -->
+		<?php
+		}
+		?>
+
+
+
+
 
 
 		<!--  start step-holder -->
