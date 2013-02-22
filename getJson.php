9db4707195478afa810eaeac4813b991a4fad989
Human <humankhoobsirat@Humans-MacBook-Pro.local>
2013-02-22 01:11:04 +0100

    changed

--- getJson.php
+++ getJson.php
@@ -19,7 +19,7 @@
 	$collection = $db->events;
 	
 	// create Current Date in MongoDate format
-	date_default_timezone_set('America/Bahia');
+	date_default_timezone_set('AMT');
 	$heute = date("Y-m-d");
 	$start = new MongoDate(strtotime($heute));
 	
