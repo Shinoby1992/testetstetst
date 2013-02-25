38dd9f9e2ed251e0b7e660b26b6d935d4cca962f
Human <humankhoobsirat@humans-macbook-pro.fritz.box>
2013-02-25 01:24:06 +0100

    as

--- getJson.php
+++ getJson.php
@@ -19,7 +19,7 @@
 	$collection = $db->events;
 	
 	// create Current Date in MongoDate format
-	date_default_timezone_set('CET');
+	date_default_timezone_set('BRT');
 	$heute = date("Y-m-d");
 	$start = new MongoDate(strtotime($heute));
 	
