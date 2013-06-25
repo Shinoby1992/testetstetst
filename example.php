98d5a4b36bebe24c4d508c1c0aaa71006a84daeb
Human Khoobsirat <Human@Humans-MacBook-Pro.local>
2013-06-25 22:41:09 +0200

    s

--- example.php
+++ example.php
@@ -6,29 +6,16 @@ $facebook = new Facebook(array(
   'appId'  => '465444696840864',
   'secret' => '653bd1649461dd10ac338a030b9d0d79',
 ));
-
-// Get User ID
 $user = $facebook->getUser();
-
-// We may or may not have this data based on whether the user is logged in.
-//
-// If we have a $user id here, it means we know the user is logged into
-// Facebook, but we don't know if the access token is valid. An access
-// token is invalid if the user logged out of Facebook.
-
 if ($user) {
   try {
     // Proceed knowing you have a logged in user who's authenticated.
     $user_profile = $facebook->api('/VillageDortmund');
-	
-	
-	
   } catch (FacebookApiException $e) {
     error_log($e);
     $user = null;
   }
 }
-
 // Login or logout url will be needed depending on current user state.
 if ($user) {
   $logoutUrl = $facebook->getLogoutUrl();
@@ -75,7 +62,9 @@ $naitik = $facebook->api('/naitik');
     <?php if ($user): ?>
       <h3>You</h3>
       <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
-
+	  
+	  <?php echo $user_profile['location']; ?>
+	  
       <h3>Your User Object (/me)</h3>
       <pre><?php print_r($user_profile); ?></pre>
     <?php else: ?>
