6ced7e27c821b129f6174d5c6df8abc11a87219b
Human Khoobsirat <Human@Humans-MacBook-Pro.local>
2013-06-25 22:56:37 +0200

    s

--- example.php
+++ example.php
@@ -66,10 +66,7 @@ $naitik = $facebook->api('/naitik');
 	  <?php echo $user_profile['location']['city']; ?>
 	  <?php echo $user_profile['location']['street']; ?>
 	  
-	  <?php echo $user_profile2['start_time']; ?>
-	  <?php echo $user_profile2['description']; ?>
-	  <?php echo $user_profile2['id']; ?>
-	  <?php echo $user_profile2['cover']['source']; ?>
+	  <?php echo $user_profile2['data']; ?>
 	  
 
       <h3>Your User Object (/me)</h3>
