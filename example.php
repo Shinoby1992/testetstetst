4e4adfd2c7a5ddb2c6c169ed538e6be507589ccc
Human Khoobsirat <Human@Humans-MacBook-Pro.local>
2013-06-25 22:37:32 +0200

    s

--- example.php
+++ example.php
@@ -1,20 +1,4 @@
 <?php
-/**
- * Copyright 2011 Facebook, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may obtain
- * a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
 require 'src/facebook.php';
 
 // Create our Application instance (replace this with your appId and secret).
@@ -35,7 +19,10 @@ $user = $facebook->getUser();
 if ($user) {
   try {
     // Proceed knowing you have a logged in user who's authenticated.
-    $user_profile = $facebook->api('/me');
+    $user_profile = $facebook->api('/VillageDortmund');
+	
+	
+	
   } catch (FacebookApiException $e) {
     error_log($e);
     $user = null;
