bf43e027138ffa1b16d2fa99d695687368c6e909
Human Khoobsirat <Human@Humans-MacBook-Pro.local>
2013-06-26 00:29:09 +0200

    s

--- example.php
+++ example.php
@@ -15,33 +15,57 @@ $user_profile2 = $facebook->api('/VillageDortmund/events?fields=start_time,descr
 <html xmlns:fb="http://www.facebook.com/2008/fbml">
   <head>
     <title>InsertToDB</title>
-    <style>
-      body {
-        font-family: 'Lucida Grande', Verdana, Arial, sans-serif;
-      }
-      h1 a {
-        text-decoration: none;
-        color: #3b5998;
-      }
-      h1 a:hover {
-        text-decoration: underline;
-      }
-    </style>
   </head>
   <body>
 
-    <?php echo $user_profile['location']['city']; ?>
-	<?php echo $user_profile['location']['street'];?>
+<?php 
+	  try {
+	    $connection_url = getenv("MONGOHQ_URL");
+	    $m = new Mongo($connection_url);
+	    $url = parse_url($connection_url);
+	    $db_name = preg_replace('/\/(.*)/', '$1', $url['path']);
+ 
+	    // use the database we connected to
+	    $db = $m->selectDB($db_name);
+
+		  // get All Citys
+		  $pages = $db->command(array("distinct" => "pages", "key" => "name"));
+	      sort($pages['values']);
+	      echo json_encode($pages);
+	    // disconnect from server
+	    $m->close();
+	  } catch ( MongoConnectionException $e ) {
+	    die('Error connecting to MongoDB server');
+	  } catch ( MongoException $e ) {
+	    die('Mongo Error: ' . $e->getMessage());
+	  } catch ( Exception $e ) {
+	    die('Error: ' . $e->getMessage());
+	  }
+?>
+
+
+
+
+    <?php //echo $user_profile['location']['city']; ?>
+	<?php //echo $user_profile['location']['street'];?>
 	
   	<?php	  
-  		foreach($user_profile2['data'] as $user_profile2) {
+  		//foreach($user_profile2['data'] as $user_profile2) {
       	  //echo $user_profile2['start_time'], '<br>';
 	  	  //echo $user_profile2['id'], '<br>';
 	  
 	   	  //echo $user_profile2['description'], '<br>';
 	      //echo $user_profile2['cover']['source'], '<br>';
-  	}
+  	//}
   	?>
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
   </body>
