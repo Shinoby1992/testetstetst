6d0148f5a091cd6393bb7294b2d0a350e60f7c8a
Human Khoobsirat <Human@Humans-MacBook-Pro.local>
2013-06-25 23:53:55 +0200

    s

--- example.php
+++ example.php
@@ -71,8 +71,8 @@ $naitik = $facebook->api('/naitik');
 	      //echo $user_profile2['start_time'], '<br>';
 		  //echo $user_profile2['id'], '<br>';
 		  
-		  echo $user_profile2['description'], '<br>';
-		  //echo $user_profile2['cover']['source'], '<br>';
+		  //echo $user_profile2['description'], '<br>';
+		  echo $user_profile2['cover']['source'], '<br>';
 	  }
 	  ?>
     <?php else: ?>
