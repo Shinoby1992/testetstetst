2ea673c67003f431190fe4dcff62c4474000df15
Human Khoobsirat <Human@Humans-MacBook-Pro.local>
2013-06-26 01:17:58 +0200

    s

--- example.php
+++ example.php
@@ -7,6 +7,32 @@ $facebook = new Facebook(array(
   'secret' => '653bd1649461dd10ac338a030b9d0d79',
 ));
 
+// Get User ID
+$user = $facebook->getUser();
+
+// We may or may not have this data based on whether the user is logged in.
+//
+// If we have a $user id here, it means we know the user is logged into
+// Facebook, but we don't know if the access token is valid. An access
+// token is invalid if the user logged out of Facebook.
+
+if ($user) {
+  try {
+    // Proceed knowing you have a logged in user who's authenticated.
+    $user_profile = $facebook->api('/rushhourdortmund?fields=location');
+  } catch (FacebookApiException $e) {
+    error_log($e);
+    $user = null;
+  }
+}
+
+// Login or logout url will be needed depending on current user state.
+if ($user) {
+  $logoutUrl = $facebook->getLogoutUrl();
+} else {
+  $loginUrl = $facebook->getLoginUrl();
+}
+
 $user_profile = $facebook->api('/rushhourdortmund?fields=location');
 $user_profile2 = $facebook->api('/VillageDortmund/events?fields=start_time,description,cover,id');
 
@@ -17,6 +43,34 @@ $user_profile2 = $facebook->api('/VillageDortmund/events?fields=start_time,descr
     <title>InsertToDB</title>
   </head>
   <body>
+      <h1>php-sdk</h1>
+
+      <?php if ($user): ?>
+        <a href="<?php echo $logoutUrl; ?>">Logout</a>
+      <?php else: ?>
+        <div>
+          Login using OAuth 2.0 handled by the PHP SDK:
+          <a href="<?php echo $loginUrl; ?>">Login with Facebook</a>
+        </div>
+      <?php endif ?>
+	  
+	  
+      <?php if ($user): ?>
+        <h3>You</h3>
+        <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
+
+        <h3>Your User Object (/me)</h3>
+        <pre><?php print_r($user_profile); ?></pre>
+      <?php else: ?>
+        <strong><em>You are not Connected.</em></strong>
+      <?php endif ?>
+	  
+	  
+	  
+	  
+	  
+	  
+	  
       <?php echo $user_profile; ?>
 
 <?php 
