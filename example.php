bfffa45fb8113e18abcdc69d8cd9f06c2c203ea8
Human Khoobsirat <Human@humans-macbook-pro.fritz.box>
2013-06-26 18:51:21 +0200

    s

--- example.php
+++ example.php
@@ -13,6 +13,8 @@ if ($user) {
   try {
     // Proceed knowing you have a logged in user who's authenticated.
     $user_profile = $facebook->api('/rushhourdortmund');
+	$user_profile2 = $facebook->api('/VillageDortmund/events?fields=start_time,description,cover,id');
+	
   } catch (FacebookApiException $e) {
     error_log($e);
     $user = null;
@@ -54,6 +56,20 @@ if ($user) {
 
 	    <?php echo $user_profile['location']['city']; ?>
 		<?php echo $user_profile['location']['street'];?>
+		
+		
+		
+	  	<?php	  
+	  		foreach($user_profile2['data'] as $user_profile2) {
+	      	  echo $user_profile2['start_time'], '<br>';
+		  	  echo $user_profile2['id'], '<br>';
+		   	  //echo $user_profile2['description'], '<br>';
+		      echo $user_profile2['cover']['source'], '<br>';
+	  	}
+	  	?>
+		
+		
+		
 
       <?php else: ?>
         <strong><em>You are not Connected.</em></strong>
