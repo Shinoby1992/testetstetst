580cbc65cbcf794f6805c74ac4913d9d2708a111
humankhoobsirat <human@gmail.com>
2016-12-13 15:25:11 +0100

    make it better

--- application/views/error/error.php
+++
@@ -1,3 +0,0 @@
-<h4><?php echo lang("ctn_133") ?></h4>
-<b><?php echo lang("ctn_134") ?></b>: <?php echo $message ?><br /><br />
-<p><input type="button" value="<?php echo lang("ctn_135") ?>" onclick="window.history.back()" class="btn btn-default btn-sm" /> </p>
