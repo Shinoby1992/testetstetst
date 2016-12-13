70b3eb4e5d724cef2f97a83137a0aa3dcb4bc8b2
humankhoobsirat <human@gmail.com>
2016-12-13 15:56:17 +0100

    make it better

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -8,6 +8,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | This file lets you define "hooks" to extend CI without hacking the core
 | files.  Please see the user guide for info:
 |
-|	https://codeigniter.com/user_guide/general/hooks.html
+|	http://codeigniter.com/user_guide/general/hooks.html
 |
 */
+$hook['pre_controller'] = array(
+            'class'    => 'Language_Switcher',
+            'function' => 'switch_lang',
+            'filename' => 'Language_Switcher.php',
+            'filepath' => 'hooks',
+            'params'   => array()
+            );
--- system/database/drivers/mysqli/mysqli_driver.php
+++ system/database/drivers/mysqli/mysqli_driver.php
@@ -125,7 +125,8 @@ class CI_DB_mysqli_driver extends CI_DB {
 		}
 		else
 		{
-			$hostname = ($persistent === TRUE)
+			// Persistent connection support was added in PHP 5.3.0
+			$hostname = ($persistent === TRUE && is_php('5.3'))
 				? 'p:'.$this->hostname : $this->hostname;
 			$port = empty($this->port) ? NULL : $this->port;
 			$socket = NULL;
@@ -183,7 +184,7 @@ class CI_DB_mysqli_driver extends CI_DB {
 					// https://bugs.php.net/bug.php?id=68344
 					elseif (defined('MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT'))
 					{
-						$client_flags |= MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT;
+						$this->_mysqli->options(MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT, TRUE);
 					}
 				}
 
@@ -255,7 +256,6 @@ class CI_DB_mysqli_driver extends CI_DB {
 		if ($this->conn_id->select_db($database))
 		{
 			$this->database = $database;
-			$this->data_cache = array();
 			return TRUE;
 		}
 
@@ -501,8 +501,8 @@ class CI_DB_mysqli_driver extends CI_DB {
 		if ( ! empty($this->_mysqli->connect_errno))
 		{
 			return array(
-				'code'    => $this->_mysqli->connect_errno,
-				'message' => $this->_mysqli->connect_error
+				'code' => $this->_mysqli->connect_errno,
+				'message' => is_php('5.2.9') ? $this->_mysqli->connect_error : mysqli_connect_error()
 			);
 		}
 
